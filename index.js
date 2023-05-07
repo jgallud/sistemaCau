1082591ca18bef6b80111573a384acf0a27a9719
Jose A. Gallud <jgallud@gmail.com>
2023-05-07 20:09:43 +0200

    api

---
+++ index.js
@@ -0,0 +1,32 @@
+const fs=require("fs");
+const express = require('express');
+const app = express();
+const modelo = require("./servidor/modelo.js");
+
+const PORT = process.env.PORT || 3000;
+
+let sistema = new modelo.Sistema();
+
+app.use(express.static(__dirname + "/"));
+
+app.get("/", function(request,response){
+  var contenido=fs.readFileSync(__dirname+"/cliente/index.html");
+  response.setHeader("Content-type","text/html");
+  response.send(contenido);
+});
+
+app.get("/agregarUsuario/:nick",function(request,response){
+  let nick = request.params.nick;
+  let res=sistema.agregarUsuario(nick);
+  response.send(res); 
+});
+
+app.get("/obtenerUsuarios",function(request,response){
+  let lista=sistema.obtenerUsuarios();
+  response.send(lista);
+});
+
+app.listen(PORT, () => {
+  console.log(`App está escuchando en el puerto ${PORT}`);
+  console.log('Ctrl+C para salir');
+});
---
+++ servidor/modelo.js
@@ -0,0 +1,107 @@
+function Sistema(){
+	this.usuarios={}
+	this.tecnicos={}
+	this.gestor=new Gestor(this);
+	this.agregarUsuario=function(nick){
+		let res={"res":"nook"};
+		if (!this.usuarios[nick]){
+				this.usuarios[nick]=new Usuario(nick);
+				res={"res":"ok"};
+		}
+		return res;
+	}
+	this.agregarTecnico=function(nick){
+		this.tecnicos[nick]=new Tecnico(nick);
+	}
+	this.crearIncidencia=function(nick,servicio){
+		let codigo=-1;
+		if (this.usuarios[nick]){
+			codigo=this.gestor.crearIncidencia(nick,servicio,this)
+		}
+		return codigo;
+	}
+	this.asignarServicio=function(nick,servicio){
+		this.tecnicos[nick].servicio=servicio;
+	}
+	this.obtenerUsuarios=function(){
+		let lista=[];
+		for (let key in this.usuarios){
+			lista.push({"nick":key});
+		}
+		return lista;
+	}
+}
+
+function Usuario(nick){
+	this.nick=nick;
+}
+
+function Tecnico(nick){
+	this.nick=nick;
+	this.servicio;
+}
+
+function Gestor(sistema){
+	this.sistema=sistema;
+	this.incidencias=[]
+	this.crearIncidencia=function(nick,servicio){
+		let codigo=-1;
+		let incidencia=new Incidencia(nick,servicio);
+		codigo=incidencia.cauNum;
+		this.incidencias.push(incidencia);
+		return codigo;
+	}
+	this.asignarTecnico=function(codigo,nickTecnico){
+		let incidencia=this.incidencias[codigo];
+		if (sistema.tecnicos[nickTecnico]){
+			incidencia.asignarTecnico(nickTecnico)
+		}
+	}
+	this.cerrarIncidencia=function(codigo,nickTecnico){
+		let incidencia=this.incidencias[codigo];
+		if (incidencia.tecnico==nickTecnico){
+			incidencia.cerrarIncidencia();
+		}
+	}
+}
+
+function Incidencia(nick,servicio){
+	this.nick=nick;
+	this.estado=new Abierta();
+	this.servicio=servicio;
+	this.cauNum=Date.now().toString();
+	this.descripcion
+	this.fecha=new Date().toDateString();
+	this.tecnico;
+	this.asignarTecnico=function(nick){
+		this.estado.asignarTecnico(nick,this);
+	}
+	this.cerrarIncidencia=function(){
+		this.estado.cerrarIncidencia(this);
+	}
+}
+
+function Abierta(){
+	this.nombre="abierta";
+	this.asignarTecnico=function(nick,incidencia){
+		incidencia.tecnico=nick;
+		incidencia.estado=new Asignada();
+	}
+	this.cerrarIncidencia=function(incidencia){}
+}
+
+function Asignada(){
+	this.nombre="asignada";
+	this.asignarTecnico=function(nick,incidencia){}
+	this.cerrarIncidencia=function(incidencia){
+		incidencia.estado=new Cerrada();
+	}
+}
+
+function Cerrada(){
+	this.nombre="cerrada";
+	this.asignarTecnico=function(nick,incidencia){}
+	this.cerrarIncidencia=function(incidencia){}
+}
+
+module.exports.Sistema=Sistema;
